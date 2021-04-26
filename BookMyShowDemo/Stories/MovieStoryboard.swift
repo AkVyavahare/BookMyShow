1f91df4537e29c76be0c9ec6b9007ed2e6db4a43
Akshay <akshay.vyavahare@mobisoftinfotech.com>
2021-04-27 00:23:22 +0530

    Added storyboard router

---
+++ BookMyShowDemo/Stories/MovieStoryboard.swift
@@ -0,0 +1,23 @@
+//
+//  MovieStoryboard.swift
+//  BookMyShowDemo
+//
+//  Created by Akshay Vyavhare on 27/04/21.
+//
+
+import Foundation
+import UIKit
+
+enum MovieStoryboard: StoryboardRouter {
+    case initial
+    // case detail
+    var name: String {
+        return "Movie"
+    }
+    
+    var controller: UIViewController {
+        switch self {
+        case .initial: return self.getInitialViewController()!
+        }
+    }
+}
